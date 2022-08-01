7a4d6902de4fb856b78acb312695dbd7c0d4ba41
Isidro Raposo <isidro.valentim.ir@gmail.com>
2022-08-01 16:09:57 +0200

    chore: implementando pagina de post

--- src/pages/CameraPage.vue
+++ src/pages/CameraPage.vue
@@ -1,13 +1,98 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5> Camera Page </h5>
+  <q-page class="constraint-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <video
+        ref="video"
+        class="full-width"
+        autoplay></video>
+      <canvas
+        ref="canvas"
+        class="full-width"
+        height="240" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        @click="captureImage"
+        v-model="post.caption"
+        color="grey-10"
+        icon="eva-camera"
+        size="lg"
+        round
+      />
+      <div class="row justify-center q-ma-md">
+        <q-input
+          class="col"
+          label="Título"
+          dense />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input
+          v-model="post.location"
+          class="col"
+          label="Localização"
+          dense>
+
+          <template v-slot:append>
+            <q-btn
+              round
+              dense
+              flat
+              icon="eva-navigation-2-outline" />
+          </template>
+
+        </q-input>
+      </div>
+      <div class="row justify-center q-mt-lg">
+        <q-btn
+          unelevated
+          rounded
+          color="primary"
+          label="Publicar Imagem" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { uid } from 'quasar'
+require('md-gum-polyfill')
 
-export default defineComponent({
-  name: 'CameraPage'
-})
+export default {
+  name: 'CameraPage',
+
+  data() {
+    return {
+      post: {
+        id: uid(),
+        caption: '',
+        location: '',
+        photo: null,
+        date: Date.now()
+      }
+    }
+  },
+  methods: {
+    initCamera() {
+      navigator.mediaDevices.getUserMedia({
+        video: true
+
+      }).then(stream => {
+        this.$refs.video.srcObject = stream
+      })
+    },
+    captureImage
+  },
+  mounted() {
+    this.initCamera()
+  }
+
+}
 </script>
+
+<style lang="sass">
+
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 15px
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,123 @@
 <template>
   <q-page class="constraint q-pa-md">
-    <h5> HomePage </h5>
+
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered>
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label
+                class="text-bold">
+                mr_isy_fox
+                </q-item-label>
+              <q-item-label caption>
+                {{ post.location }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-img :src="post.imageUrl"/>
+
+          <q-card-section>
+            <div> {{ post.caption }} </div>
+            <div class="text-caption text-grey"> {{ niceDate(post.date) }} </div>
+          </q-card-section>
+
+        </q-card>
+      </div>
+      <div
+        class="col-4 large-screen-only">
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label
+              class="text-bold">
+              mr_isy_fox
+            </q-item-label>
+            <q-item-label caption>
+              Isidro Raposo
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+        </div>
+    </div>
+
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'HomePage'
-})
+import { date } from 'quasar'
+
+export default {
+  name: 'HomePage',
+
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: 'Golden Gate Bridge',
+          date: 1497159857411,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg',
+        },
+        {
+          id: 2,
+          caption: 'Golden Gate Bridge',
+          date: 1497159857411,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg',
+        },
+        {
+          id: 3,
+          caption: 'Golden Gate Bridge',
+          date: 1497159857411,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg',
+        },
+        {
+          id: 4,
+          caption: 'Golden Gate Bridge',
+          date: 1497159857411,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg',
+        }
+      ]
+    }
+  },
+
+  methods: {
+    niceDate(value) {
+      return date.formatDate(value, 'DD MMMM YYYY HH:mm')
+    }
+  }
+
+}
 </script>
+
+<style lang="sass">
+
+.card-post
+  .q-img
+    min-height: 200px
+
+</style>
